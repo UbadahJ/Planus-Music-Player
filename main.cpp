676de716c39b5bc1a08ebd6a12d7d7141bd15093
UbadahJ <ubadahjafry@gmail.com>
2019-12-05 01:21:28 +0500

    Initial working music prototype

--- main.cpp
+++ main.cpp
@@ -1,56 +1,32 @@
-#include <iostream>
-#include <ao/ao.h>
-#include <mpg123.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_mixer.h"
 
-const int BITS{8};
+static const char *MY_COOL_MP3 = "/home/one/Music/Hero_Memories.mp3";
 
-using namespace std;
+int main(int argc, char **argv) {
+    int result = 0;
+    int flags = MIX_INIT_MP3;
 
-int main() {
-    mpg123_handle *handle;
-    unsigned char *buffer;
-    size_t buffer_size, done;
-    int err;
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("Failed to init SDL\n");
+        exit(1);
+    }
 
-    int driver;
-    ao_device *device;
-    ao_sample_format sampleFormat;
-    int channel, encoding;
-    long rate;
+    if (flags != (result = Mix_Init(flags))) {
+        printf("Could not initialize mixer (result: %d).\n", result);
+        printf("Mix_Init: %s\n", Mix_GetError());
+        exit(1);
+    }
 
-    ao_initialize();
-    driver = ao_default_driver_id();
-    mpg123_init();
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
+    Mix_Music *music = Mix_LoadMUS(MY_COOL_MP3);
+    Mix_PlayMusic(music, 1);
 
-    handle = mpg123_new(nullptr, &err);
-    buffer_size = mpg123_outblock(handle);
+    while (!SDL_QuitRequested()) {
+        SDL_Delay(250);
+    }
 
-    buffer = new unsigned char[buffer_size];
-    mpg123_open(handle, "/home/one/Music/Hero_Memories.mp3");
-    mpg123_getformat(handle, &rate, &channel, &encoding);
-
-    sampleFormat.bits = mpg123_encsize(encoding) * BITS;
-    sampleFormat.rate = rate;
-    sampleFormat.channels = channel;
-    sampleFormat.byte_format = AO_FMT_NATIVE;
-    sampleFormat.matrix = nullptr;
-    device = ao_open_live(driver, &sampleFormat, nullptr);
-
-    cout << "Driver id: " << driver << '\t';
-    cout << "Device id: " << device << endl;
-    cout << "Rate: " << rate << '\t' << "Channel: " << channel << '\t' << "Encoding: " << encoding << endl;
-    cout << "Error No: " << errno << endl;
-
-    while (mpg123_read(handle, buffer, buffer_size, &done) == MPG123_OK)
-        ao_play(device, reinterpret_cast<char *>(buffer), done);
-
-    delete (buffer);
-
-    ao_close(device);
-    mpg123_close(handle);
-    mpg123_delete(handle);
-    mpg123_exit();
-    ao_shutdown();
-
+    Mix_FreeMusic(music);
+    SDL_Quit();
     return 0;
 }
